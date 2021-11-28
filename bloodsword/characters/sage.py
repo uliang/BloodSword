ec6c9995724f82cdd39bccf524209e13a01cae40
uliang <uliang6482@gmail.com>
2021-11-28 09:38:59 +0800

    Update sage.py
    Added rank attribute progressions for Sage class

--- bloodsword/characters/sage.py
+++ bloodsword/characters/sage.py
@@ -3,7 +3,8 @@ from ..base import Character
 
 
 class Sage(Character):
-    fighting_prowess = Attribute()
-    psychic_ability = Attribute()
-    awareness = Attribute()
-    endurance = Attribute(has_max_value=True)
+    fighting_prowess = Attribute(rank_attributes=[7, 7, 7, 7, 7, 7, 8])
+    psychic_ability = Attribute(rank_attributes=[7, 7, 8, 8, 8, 8, 8])
+    awareness = Attribute(rank_attributes=[6, 6, 7, 7, 7, 7, 7])
+    endurance = Attribute(has_max_value=True,
+                          rank_attributes=[10, 15, 20, 25, 30, 35, 40])
