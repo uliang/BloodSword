8af8ca97dd40865222e4dd01f6af53e3981fb7a9
uliang <uliang6482@gmail.com>
2021-11-28 09:40:20 +0800

    Update test_characters.py
    Added tests for from_rank class method

--- tests/test_characters.py
+++ tests/test_characters.py
@@ -19,6 +19,24 @@ def test_character_initialization(warrior, sage):
         sage) == "<Name:Sage Fighting Prowess=7 Psychic Ability=7 Awareness=6 Endurance=10>"
 
 
+def test_character_from_rank():
+    warrior = Warrior.from_rank("Warrior", 2)
+    assert str(
+        warrior) == "<Name:Warrior Fighting Prowess=8 Psychic Ability=6 Awareness=6 Endurance=12>"
+
+    sage = Sage.from_rank("Sage", 2)
+    assert str(
+        sage) == "<Name:Sage Fighting Prowess=7 Psychic Ability=7 Awareness=6 Endurance=10>"
+
+    warrior8 = Warrior.from_rank("Warrior", 8)
+    assert str(
+        warrior8) == "<Name:Warrior Fighting Prowess=9 Psychic Ability=7 Awareness=7 Endurance=48>"
+
+    sage8 = Sage.from_rank("Sage", 8)
+    assert str(
+        sage8) == "<Name:Sage Fighting Prowess=8 Psychic Ability=8 Awareness=7 Endurance=40>"
+
+
 def test_endurance_cannot_exceed_max(warrior):
     warrior.endurance = 13
     assert warrior.endurance == 12
