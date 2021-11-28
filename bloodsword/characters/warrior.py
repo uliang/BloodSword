20728dad8087dd163c6c3c840770f2f95c867c8b
uliang <uliang6482@gmail.com>
2021-11-28 09:39:25 +0800

    Update warrior.py
    Added rank attribute progressions for Warrior class

--- bloodsword/characters/warrior.py
+++ bloodsword/characters/warrior.py
@@ -1,8 +1,10 @@
 from ..base import Character
 from ..descriptors.attributes import Attribute
 
-class Warrior(Character): 
-    fighting_prowess = Attribute()
-    psychic_ability = Attribute()
-    awareness = Attribute()
-    endurance = Attribute(has_max_value=True)
+
+class Warrior(Character):
+    fighting_prowess = Attribute(rank_attributes=[8, 8, 8, 8, 8, 8, 9])
+    psychic_ability = Attribute(rank_attributes=[6, 6, 6, 6, 6, 6, 7])
+    awareness = Attribute(rank_attributes=[6, 6, 7, 7, 7, 7, 7])
+    endurance = Attribute(has_max_value=True,
+                          rank_attributes=[12, 18, 24, 30, 36, 42, 48])
