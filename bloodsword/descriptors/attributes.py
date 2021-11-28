30c06729ef372011ac8b005e513aef3718c6a66c
uliang <uliang6482@gmail.com>
2021-11-28 14:34:00 +0800

    Update attributes.py
    Refactor Attribute descriptor to use more straightforward logic instead of using states.

--- bloodsword/descriptors/attributes.py
+++ bloodsword/descriptors/attributes.py
@@ -1,11 +1,5 @@
 from dataclasses import dataclass, field
-from enum import Enum, auto
 from typing import List
-
-
-class AttrState(Enum):
-    UNBOUNDED = auto()
-    BOUNDED = auto()
 
 
 @dataclass
@@ -14,26 +8,19 @@ class Attribute:
     min_value: int = 0
     rank_attributes: List[int] = field(default_factory=list)
 
-    def __set_name__(self, instance, name):
+    def __set_name__(self, owner, name):
         self.private_name = '_' + name
         self.initial_name = f'_initial_{name}_value'
-        self.attribute_state = f'_{name}_state'
-        setattr(instance, self.attribute_state, AttrState.UNBOUNDED)
 
     def __get__(self, instance, type_=None):
         return getattr(instance, self.private_name)
 
     def __set__(self, instance, value):
         value = max(self.min_value, value)
-        attribute_state = getattr(instance, self.attribute_state)
-        if attribute_state is AttrState.UNBOUNDED:
-            setattr(instance, self.private_name, value)
-            if self.has_max_value:
-                setattr(instance, self.initial_name, value)
-                setattr(instance, self.attribute_state,  AttrState.BOUNDED)
-        elif attribute_state is AttrState.BOUNDED:
+        if self.has_max_value:
             initial_value = getattr(instance, self.initial_name)
-            setattr(instance, self.private_name, min(initial_value, value))
+            value = min(value, initial_value)
+        setattr(instance, self.private_name, value)
 
     def get_rank_attribute_value(self, rank:int)-> int: 
         return self.rank_attributes[rank-2]
