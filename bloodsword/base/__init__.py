04bbd036dd33f397b1f71de0e6bbcce47c31746f
uliang <uliang6482@gmail.com>
2021-11-28 09:32:27 +0800

    Update __init__.py
    Removed from_tuple class method as it will not be used for initialization

--- bloodsword/base/__init__.py
+++ bloodsword/base/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from typing import Tuple
 
 
     def __init__(self, name: str, fighting_prowess: int, psychic_ability: int, awareness: int, endurance: int):
@@ -11,8 +10,6 @@ from typing import Tuple
         self.endurance = endurance
 
     @classmethod
-    def from_tuple(cls, name, tup: Tuple[int, int, int, int]) -> Character:
-        return cls(name, *tup)
 
     def __str__(self) -> str:
         return f"Character(class:{self.name} Fighting Prowess={self.fighting_prowess} Psychic Ability={self.psychic_ability} Awareness={self.awareness} Endurance={self.endurance})"
