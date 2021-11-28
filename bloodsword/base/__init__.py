9aa06016d824cc42b9dd87073ad74a64a6181ede
uliang <uliang6482@gmail.com>
2021-11-28 09:36:43 +0800

    Update __init__.py
    Wrote from_rank class method to obtain a character of given rank drawing attribute scores from Attribute descriptor

--- bloodsword/base/__init__.py
+++ bloodsword/base/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from bloodsword.descriptors.attributes import Attribute
 
 
     def __init__(self, name: str, fighting_prowess: int, psychic_ability: int, awareness: int, endurance: int):
@@ -10,6 +11,10 @@ from __future__ import annotations
         self.endurance = endurance
 
     @classmethod
+    def from_rank(cls, name: str, rank: int = 2) -> Character:
+        kwargs = {k: v.get_rank_attribute_value(
+            rank) for k, v in cls.__dict__.items() if isinstance(v, Attribute)}
+        return cls(name, **kwargs)
 
     def __str__(self) -> str:
         return f"Character(class:{self.name} Fighting Prowess={self.fighting_prowess} Psychic Ability={self.psychic_ability} Awareness={self.awareness} Endurance={self.endurance})"
