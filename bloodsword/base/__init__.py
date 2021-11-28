fa6825e57f3a7706be6cef24764122f386818a62
uliang <uliang6482@gmail.com>
2021-11-28 14:47:35 +0800

    Update __init__.py
    use the vars function instead of cls.__dict__

--- bloodsword/base/__init__.py
+++ bloodsword/base/__init__.py
@@ -45,7 +45,7 @@ class Character(ABC):
     @classmethod
     def from_rank(cls, name: str, rank: int = 2) -> Character:
         kwargs = {k: v.get_rank_attribute_value(
-            rank) for k, v in cls.__dict__.items() if isinstance(v, Progressable)}
+            rank) for k, v in vars(cls).items() if isinstance(v, Progressable)}
         return cls(name, **kwargs)
 
     def store_item(self, item) -> None:
