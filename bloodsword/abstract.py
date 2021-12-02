b00bccd22553723fdad13478ff3814164697cc91
Tang U-Liang <uliang6482@gmail.com>
2021-12-02 22:58:06 +0800

    Update abstract.py
    Added TacticalMap abstract class

--- bloodsword/abstract.py
+++ bloodsword/abstract.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractclassmethod, abstractmethod
 from dataclasses import dataclass, field
 from typing import Any
 
@@ -17,6 +17,75 @@ class Position:
     """
     x: int = 0
     y: int = 0
+
+
+@dataclass
+class TacticalMap(ABC):
+    """
+    Abstract class to implement logic for acquiring data of valid sqaures
+    which a character can pass through, obstacles and squares which monsters
+    but not characters may pass through. 
+
+    .. py:attribute:: grids
+        :type: List[Position]
+
+        Players may pass through these squares. 
+
+    .. py:attribute:: obstacles
+        :type: List[Position]
+
+        Neither monsters nor players can pass through these squares.
+
+    .. py:attribute:: partial_obstacles
+        :type: List[Position]
+
+        Monsters may pass through these squares but not players. 
+
+    .. py:attribute:: flee 
+        :type: List[Position] 
+
+        Player may flee only when on this square or is adjacent to a player adjacent or
+        on a fleeing square. 
+    """
+    grids: List[Position]
+    obstacles: List[Position]
+    partial_obstacles: List[Position]
+    flee: List[Position]
+
+    @abstractclassmethod
+    def from_listoflists(cls, diagram: List[List[str]]) -> None:
+        """
+        Implement parsing logic for string representation of grid. 
+
+        For example, ::
+
+            >>> diagram = [
+                    ['o', 'o'], 
+                    ['*', 'x'],
+                    ['*', 'f']
+                ]
+            >>> map = TacticalMap.from_listoflists(diagram)
+            >>> map.grids 
+            [(0,0), (0, 1)]
+            >>> map.obstacles
+            [(1,1)]
+            >>> map.partial_obstacles
+            [(1,0), (2,0)]   
+            >>> map.flee
+            [(2, 1)]
+        """
+
+    @abstractmethod
+    def valid_destination(self, destination: Position) -> bool:
+        """
+        Determines if destination can be moved into
+        """
+
+    @abstractmethod
+    def compute_nearest(self, pc_positions: List[Position], monster_position: Position) -> Position:
+        """
+        Determines nearest player character to monster.
+        """
 
 
 @dataclass
