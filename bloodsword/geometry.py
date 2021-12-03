ef987edfe308d0a598102052f9f9c95f1ff433f1
Tang U-Liang <uliang6482@gmail.com>
2021-12-04 07:29:17 +0800

    Update geometry.py
    Moved TacticalMap class to geometry module

--- bloodsword/geometry.py
+++ bloodsword/geometry.py
@@ -38,3 +38,59 @@ class Position:
     def square(self, value: Square):
         self._square_type = value
 
+
+@dataclass
+class TacticalMap:
+    """
+    Representation of valid sqaures
+    which a character can pass through, obstacles and squares which monsters
+    but not characters may pass through. 
+
+    .. py:attribute:: grids
+        :type: List[Position]
+
+        Players may pass through these squares. 
+
+    .. py:attribute:: obstacles
+        :type: List[Position]
+
+        Neither monsters nor players can pass through these squares.
+
+    .. py:attribute:: partial_obstacles
+        :type: List[Position]
+
+        Monsters may pass through these squares but not players. 
+
+    .. py:attribute:: flee 
+        :type: List[Position] 
+
+        Player may flee only when on this square or is adjacent to a player adjacent or
+        on a fleeing square. 
+    """
+    grids: List[Position]
+    obstacles: List[Position]
+    partial_obstacles: List[Position]
+    flee: List[Position]
+
+    def from_listoflists(cls, diagram: List[List[str]]) -> None:
+        """
+        Parsing logic for string representation of grid. 
+
+        For example, ::
+
+            >>> diagram = [
+                    ['o', 'o'], 
+                    ['*', 'x'],
+                    ['*', 'f']
+                ]
+            >>> map = TacticalMap.from_listoflists(diagram)
+            >>> map.grids 
+            [(0,0), (0, 1)]
+            >>> map.obstacles
+            [(1,1)]
+            >>> map.partial_obstacles
+            [(1,0), (2,0)]   
+            >>> map.flee
+            [(2, 1)]
+        """
+
