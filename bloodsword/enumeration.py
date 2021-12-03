ce0db20f2142e86307617f52bf967c4e2c93a5bc
Tang U-Liang <uliang6482@gmail.com>
2021-12-04 07:24:54 +0800

    Update enumeration.py
    Added enums for character attribute fields

--- bloodsword/enumeration.py
+++ bloodsword/enumeration.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class CharacterClass(str, Enum):
@@ -6,6 +6,7 @@ class CharacterClass(str, Enum):
     TRICKSTER = 'trickster'
     SAGE = 'sage'
     ENCHANTER = 'enchanter'
+    MONSTER = 'monster'
 
 
 class Spell(str, Enum):
@@ -13,4 +14,9 @@ class Spell(str, Enum):
 
 
 class Attribute(str, Enum):
-    ...
+    XP = 'xp'
+    FIGHTTING_PROWESS = 'fighting_prowess' 
+    PSYCHIC_ABILITY = 'psychic_ability' 
+    AWARENESS = 'awareness' 
+    ENDURANCE = 'endurance'
+    DAMAGE = 'damage'
