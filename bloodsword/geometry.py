48d2040dd9ff5a7cb906d7ef4bdfd73cd1b4e531
Tang U-Liang <uliang6482@gmail.com>
2021-12-09 21:57:04 +0800

    Refactoring geometry
    Moved Position into value

--- bloodsword/geometry.py
+++ bloodsword/geometry.py
@@ -7,39 +7,6 @@ from .enumeration import Square
 
 
 @dataclass
-class Position:
-    """
-    Position on a tactical map. 
-
-    We follow the convention where the origin is located at the top left corner
-    of the map. 
-    """
-    x: int = 0
-    y: int = 0
-
-    _square_type: Square = field(default=Square.GRID, init=False)
-
-    def __repr__(self):
-        return repr((self.x, self.y))
-
-    def is_adjacent(self, to: Position) -> bool:
-        """
-        :return: Returns ``True`` if ``to`` position is on square away in the cardinal directions and ``False`` otherwise.
-        :rtype: bool
-        """
-
-    @property
-    def square(self) -> Square:
-        """
-        Denotes the type of square the coordinates are referencing."""
-        return self._square_type
-
-    @square.setter
-    def square(self, value: Square):
-        self._square_type = value
-
-
-@dataclass
 class TacticalMap:
     """
     Representation of valid sqaures
