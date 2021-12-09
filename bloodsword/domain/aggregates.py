ac461403a08708c65535d30053f2c3ff544717c5
Tang U-Liang <uliang6482@gmail.com>
2021-12-09 21:59:47 +0800

    Refactor geometry
    Moved TacticalMap into aggregates

--- bloodsword/domain/aggregates.py
+++ bloodsword/domain/aggregates.py
@@ -78,3 +78,48 @@ class Character(ABC):
         score. If the casting failed, the same spell may be cast again by reducing roll result by one. 
 
         The casting roll is reset once a spell is cast successfully."""
+
+
+@dataclass
+class TacticalMap:
+    """
+    Representation of valid sqaures
+    which a character can pass through, obstacles and squares which monsters
+    but not characters may pass through. 
+
+    .. py:attribute:: grids
+        :type: List[Position]
+
+        Players may pass through these squares. 
+
+    .. py:attribute:: obstacles
+        :type: List[Position]
+
+        Neither monsters nor players can pass through these squares.
+
+    .. py:attribute:: partial_obstacles
+        :type: List[Position]
+
+        Monsters may pass through these squares but not players. 
+
+    .. py:attribute:: flee 
+        :type: List[Position] 
+
+        Player may flee only when on this square or is adjacent to a player adjacent or
+        on a fleeing square. 
+    """
+    grids: List[Position]
+    obstacles: List[Position]
+    partial_obstacles: List[Position]
+    flee: List[Position]
+
+    @abstractmethod
+    def valid_destination(self, destination: Position) -> bool:
+        """
+        Determines if destination can be moved into
+        """
+    @abstractmethod
+    def compute_nearest(self, pc_positions: List[Position], monster_position: Position) -> Position:
+        """
+        Determines nearest player character to monster.
+        """
